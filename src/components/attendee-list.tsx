f80ff41ec749ad39f71435738bdd9857605d691c
raissa ecker <raissa.ecker.re@gmail.com>
2024-04-03 23:24:04 -0300

    commit aula 03

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -7,20 +7,49 @@ import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
 import { TableRow } from "./table/table-row";
-import { ChangeEvent, useState } from "react";
-import { attendees } from "../data/attendees";
+import { ChangeEvent, useEffect, useState } from "react";
 
 dayjs.extend(relativeTime);
 dayjs.locale("pt-br");
+
+interface Attendee {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+    checkedInAt: string | null;
+}
 
 export function AttendeeList() {
     const [search, setSearch] = useState('')
     const [page, setPage] = useState(1)
 
-    const totalPages = Math.ceil(attendees.length / 10)
+    const [total, setTotal] = useState(0)
+    const [attendees, setAttendees] = useState<Attendee[]>([])
+
+    const totalPages = Math.ceil(total / 10)
+
+    useEffect(() => {
+        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+        url.searchParams.set('pageIndex', String(page - 1))
+
+        if (search.length > 0) {
+            url.searchParams.set('query', search)
+        }
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setAttendees(data.attendees)
+                setTotal(data.total)
+            })
+    }, [page, search])
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
         setSearch(event.target.value)
+        setPage(1)
     }
 
     function goToNextPage() {
@@ -45,10 +74,10 @@ export function AttendeeList() {
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className="size-4 text-emerald-300" />
-                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+                    <input onChange={onSearchInputChanged} 
+                    className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" 
+                    placeholder="Buscar participante..." />
                 </div>
-
-                {search}
             </div>
 
             <Table>
@@ -65,7 +94,7 @@ export function AttendeeList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
+                    {attendees.map((attendee) => {
                         return (
                             <TableRow key={attendee.id}>
                                 <TableCell>
@@ -79,7 +108,11 @@ export function AttendeeList() {
                                     </div>
                                 </TableCell>
                                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                                <TableCell>
+                                    {attendee.checkedInAt === null ? 
+                                    <span className="text-zinc-400">Não fez check-in</span>
+                                    : dayjs().to(attendee.checkedInAt)}
+                                    </TableCell>
                                 <TableCell>
                                     <IconButton transparent={true}>
                                         <MoreHorizontal className="size-4" />
@@ -92,7 +125,7 @@ export function AttendeeList() {
                 <tfoot>
                     <tr>
                         <TableCell className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-                            Mostrando 10 de {attendees.length} itens
+                            Mostrando {attendees.length} de {total} itens
                         </TableCell>
                         <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
                             <div className="inline-flex items-center gap-8">
